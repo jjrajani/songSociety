cfe51504bd0f8f83ca2caf543173ee9add2403eb
humdrum <jjrajani@gmail.com>
2017-08-15 23:37:45 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
+/*
+  Declaring PORT so that we may dynamically change
+  which PORT to listen to. || 5000 for dev environment
+*/
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log('App Listening on PORT 5000');
+});
