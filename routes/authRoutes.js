f11660d573cea7f0585e1fb92a54a6d601d4e5c3
humdrum <jjrajani@gmail.com>
2017-08-16 16:03:20 -0400

    add facebook login procedure

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 
 module.exports = app => {
-  /* Kick Off Auth Process */
+  /* Kick Off Google Auth Process */
   app.get(
     '/auth/google',
     passport.authenticate('google', {
@@ -9,6 +9,17 @@ module.exports = app => {
     })
   );
 
-  /* Complete Auth Process */
+  /* Complete Google Auth Process */
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  /* Kick Off Facebook Auth Process */
+  app.get(
+    '/auth/facebook',
+    passport.authenticate('facebook', {
+      scope: ['public_profile', 'email']
+    })
+  );
+
+  /* Complete Facebook Auth Process */
+  app.get('/auth/facebook/callback', passport.authenticate('facebook'));
 };
--- services/passport.js
+++ services/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
 
 const keys = require('../config/keys');
 
@@ -11,9 +12,24 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('refreshToken', refreshToken);
-      console.log('profile', profile);
+      console.log('google access token', accessToken);
+      console.log('google refreshToken', refreshToken);
+      console.log('google profile', profile);
+    }
+  )
+);
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: keys.facebookClientID,
+      clientSecret: keys.facebookClientSecret,
+      callbackURL: '/auth/facebook/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('facebook access token', accessToken);
+      console.log('facebook refreshToken', refreshToken);
+      console.log('facebook profile', profile);
     }
   )
 );
