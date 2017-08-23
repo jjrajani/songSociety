63694f5c607cb8bad2f3569bc24f5ed00fc23a8b
humdrum <jjrajani@gmail.com>
2017-08-22 23:42:49 -0400

    container div. install axios, redux-thunk

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   render() {
     return (
-      <div>
-        <h2>Header</h2>
-      </div>
+      <nav>
+        <div className="nav-wrapper">
+          <Link to="/" className="left brand-logo">
+            Emaily
+          </Link>
+          <ul className="right">
+            <li>
+              <Link to="auth/google">Login With Google</Link>
+            </li>
+            <li>
+              <Link to="auth/facebook">Login With Facebook</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     );
   }
 }
