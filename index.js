18a6b05d8ae0f6aa082273b40872de307c70d7f7
humdrum <jjrajani@gmail.com>
2017-08-30 18:05:20 -0400

    app index route typo

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === 'production') {
   // recognize route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'cliend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 app.get('/', (req, res) => {
