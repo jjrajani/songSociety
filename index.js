d48f1add3442c75125c975c576cb0e410db6c7aa
nael <nerdupgeekdown@gmail.com>
2017-09-28 19:50:09 -0400

    changed server port back to 5000

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(express.static('client/build'));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log('App Listening on PORT 3000');
+    console.log('App Listening on PORT 5000');
 });
