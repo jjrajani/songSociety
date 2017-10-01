bdf8ac1b26e4fdd65e3839c8470fd2a16594b158
humdrum <jjrajani@gmail.com>
2017-10-01 10:13:22 -0400

    yarn install

--- client/src/components/Workspace/components/Comments/components/AddComment.js
+++ client/src/components/Workspace/components/Comments/components/AddComment.js
@@ -8,57 +8,75 @@ import aws from '../../../../../utils/aws';
 // Components
 
 class AddComment extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { value: '' };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-    this.handleFileUpload = this.handleFileUpload.bind(this);
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.handleFileUpload = this.handleFileUpload.bind(this);
+    }
 
-  }
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
+    handleClick(event) {
+        const { workspaceId } = this.props.match.params;
+        let userId = this.props.getProfile().then(user => {
+            console.log(user);
+            this.props.addComment(
+                user.sub,
+                this.state.value,
+                null,
+                workspaceId
+            );
+        });
+    }
 
-  handleClick(event) {
-    const { workspaceId } = this.props.match.params;
-    let userId = this.props.getProfile().then((user) => {
-      console.log(user);
-      this.props.addComment(user.sub, this.state.value, null, workspaceId);
-    });
-  }
+    handleFileUpload(event) {
+        console.log('Selected file:', event.target.files[0]);
+        aws.upload('bob', event.target.files[0], this.props.getIdToken());
+    }
 
-  handleFileUpload(event) {
-    console.log('Selected file:', event.target.files[0]);
-    aws.upload("bob", event.target.files[0], this.props.getIdToken());
-  }
-
-  render() {
-
-    return (
-      <div className="comments_wrapper">
-        <input type="text" value={this.state.value}
-          onChange={this.handleChange} />
-        <div onClick={this.handleClick}>Add Comment</div>
-        <FileInput name="myImage"
-          accept=".png,.gif"
-          onChange={this.handleFileUpload} />
-        <input type="file"
-          id="selectedFile"
-          style={{ position: 'absolute', top: '0px', left: '0px', opacity: 0, width: '100%', 'zIndex': 1 }}
-          onChange={this.handleFileUpload} />
-        <label htmlFor="selectedFile" className="glyphicon glyphicon-cloud-upload" style={{ color: 'white' }} />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="comments_wrapper">
+                <input
+                    type="text"
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                />
+                <div onClick={this.handleClick}>Add Comment</div>
+                <FileInput
+                    name="myImage"
+                    accept=".png,.gif"
+                    onChange={this.handleFileUpload}
+                />
+                <input
+                    type="file"
+                    id="selectedFile"
+                    style={{
+                        position: 'absolute',
+                        top: '0px',
+                        left: '0px',
+                        opacity: 0,
+                        width: '100%',
+                        zIndex: 1
+                    }}
+                    onChange={this.handleFileUpload}
+                />
+                <label
+                    htmlFor="selectedFile"
+                    className="glyphicon glyphicon-cloud-upload"
+                    style={{ color: 'white' }}
+                />
+            </div>
+        );
+    }
 }
 
-
-
-export default connect(null,
-  {
+export default connect(null, {
     addComment: actions.commentsActions.addComment,
     getProfile: actions.authActions.getProfile,
     getIdToken: actions.authActions.getIdToken
-  })(withRouter(AddComment));
+})(withRouter(AddComment));
