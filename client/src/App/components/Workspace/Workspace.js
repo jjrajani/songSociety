c6e39f3389659eb88afe50fc807efdc4b6157292
humdrum <jjrajani@gmail.com>
2017-10-03 01:23:39 -0400

    audio controls

--- client/src/App/components/Workspace/Workspace.js
+++ client/src/App/components/Workspace/Workspace.js
@@ -15,6 +15,7 @@ class Workspaces extends Component {
             const { workspaceId } = this.props.match.params;
             this.props.fetchWorkspace(workspaceId);
         }
+        console.log('this', this);
     }
     componentWillMount() {
         this.props.getProfile();
@@ -28,6 +29,38 @@ class Workspaces extends Component {
         let player = document.getElementById('workspace_audio_player');
         player.pause();
     }
+    restartAudio() {
+        let player = document.getElementById('workspace_audio_player');
+        player.currentTime = 0;
+    }
+    rewindAudio() {
+        let player = document.getElementById('workspace_audio_player');
+        if (!player.paused) {
+            let newTime = player.currentTime - 5;
+            player.currentTime = newTime < 0 ? 0 : newTime;
+        }
+    }
+    fastRewindAudio() {
+        let player = document.getElementById('workspace_audio_player');
+        if (!player.paused) {
+            let newTime = player.currentTime - 15;
+            player.currentTime = newTime < 0 ? 0 : newTime;
+        }
+    }
+    forwardAudio() {
+        let player = document.getElementById('workspace_audio_player');
+        if (!player.paused) {
+            let newTime = player.currentTime + 5;
+            player.currentTime = newTime < 0 ? 0 : newTime;
+        }
+    }
+    fastForwardAudio() {
+        let player = document.getElementById('workspace_audio_player');
+        if (!player.paused) {
+            let newTime = player.currentTime + 15;
+            player.currentTime = newTime < 0 ? 0 : newTime;
+        }
+    }
     render() {
         return (
             <div className="container main_content workspace">
@@ -38,12 +71,49 @@ class Workspaces extends Component {
                             <Glyphicon
                                 glyph="play"
                                 onClick={this.playAudio.bind(this)}
+                                title="Play"
                             />
                         </div>
                         <div className="pause_button">
                             <Glyphicon
                                 glyph="pause"
                                 onClick={this.pauseAudio.bind(this)}
+                                title="Pause"
+                            />
+                        </div>
+                        <div className="fast-rewind_button">
+                            <Glyphicon
+                                glyph="fast-backward"
+                                onClick={this.fastRewindAudio.bind(this)}
+                                title="Rewind"
+                            />
+                        </div>
+                        <div className="rewind_button">
+                            <Glyphicon
+                                glyph="step-backward"
+                                onClick={this.rewindAudio.bind(this)}
+                                title="Step Back"
+                            />
+                        </div>
+                        <div className="restart_button">
+                            <Glyphicon
+                                glyph="backward"
+                                onClick={this.restartAudio.bind(this)}
+                                title="Restart"
+                            />
+                        </div>
+                        <div className="forward_button">
+                            <Glyphicon
+                                glyph="step-forward"
+                                onClick={this.forwardAudio.bind(this)}
+                                title="Step Forward"
+                            />
+                        </div>
+                        <div className="fast-forward_button">
+                            <Glyphicon
+                                glyph="fast-forward"
+                                onClick={this.fastForwardAudio.bind(this)}
+                                title="Fast Forward"
                             />
                         </div>
                     </div>
