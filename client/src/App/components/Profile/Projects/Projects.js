5427cedfacd0f2870d5f79ab8dc4bc9e5da8755c
humdrum <jjrajani@gmail.com>
2017-10-06 09:54:14 -0400

    conditionals for new workspace button based on storageSize availability

--- client/src/App/components/Profile/Projects/Projects.js
+++ client/src/App/components/Profile/Projects/Projects.js
@@ -4,22 +4,27 @@ import { connect } from 'react-redux';
 // Components
 import ProjectList from './components/ProjectList';
 import { Link } from 'react-router-dom';
+import StripePay from '../../StripePay/StripePay';
 
 const Projects = ({ profile, projects }) => {
     const { paid, storageSize } = profile;
     const usedStorage = 0.8 * projects.myProjects.length;
-
+    const hasStorage = storageSize - usedStorage > 0.8;
     return (
         <div className="projects">
             <div className="header">
                 <h3>Projects</h3>
-                {paid &&
+                {paid === true &&
                     <p>
                         {usedStorage}GB / {storageSize}GB
                     </p>}
-                <Link className="btn btn-danger" to="/workspace/new">
-                    New Project
-                </Link>
+                {storageSize > 0 &&
+                    hasStorage &&
+                    <Link className="btn btn-danger" to="/workspace/new">
+                        New Project
+                    </Link>}
+                {paid === true && !hasStorage && <p>You need more storage</p>}
+                {!paid && <p>Upgrade your account to make new Projects</p>}
             </div>
             <ProjectList />
         </div>
